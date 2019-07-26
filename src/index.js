f31ae9067bd914c0fd2b32654a956852578384fd
tomi0505 <tomaszgrzyb.online@gmail.com>
2019-07-26 15:34:12 +0200

    The header works.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import './scss/style.scss';
 import chromeMobile100Vh from './js/chromeMobile100Vh';
 import scrollToAnchor from "./js/scrollToAnchor";
 import hamburgerBtn from "./js/hamburgerBtn";
+import aboutMeTypingText from "./js/aboutMeTypingText";
 
 document.addEventListener("DOMContentLoaded", () => {
   chromeMobile100Vh();
   window.addEventListener('resize', chromeMobile100Vh, false);
   scrollToAnchor();
   hamburgerBtn();
+  aboutMeTypingText();
 }, false);
--- src/js/hamburgerBtn.js
+++ src/js/hamburgerBtn.js
@@ -1,8 +1,14 @@
 const hamburgerBtn = function() {
-  const hamburgerBtn = document.querySelector('#hamburgerBtn');
+  const hamburgerBtn = document.querySelector('.hamburger-btn');
+  const hamburgerBtnInnerLineItems = document.querySelectorAll('.hamburger-btn__inner-line-item');
 
   hamburgerBtn.addEventListener('click', function() {
-    this.classList.toggle('is-active');
+    this.classList.toggle('active');
+    setTimeout(() => {
+      for(let innerLineItem of hamburgerBtnInnerLineItems) {
+        innerLineItem.classList.toggle('clicked');
+      }
+    }, 400);
   }, false);
 }
 
