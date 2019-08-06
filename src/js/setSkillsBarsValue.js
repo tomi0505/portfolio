0f7d235b6b3831eab598d76427336309ee57d26e
tomi0505 <tomaszgrzyb.online@gmail.com>
2019-08-06 15:35:08 +0200

    before start portfolio section again.

--- src/js/setSkillsBarsValue.js
+++ src/js/setSkillsBarsValue.js
@@ -1,10 +1,9 @@
 const setSkillsBarsValue = function() {
-  const barsEl = document.querySelectorAll('.skills-container .bar');
+  const barsProgressEl = document.querySelectorAll('.skills-container .bar .bar__progress');
 
-  const skills = [78, 62, 51, 65, 40, 71, 52, 29, 17, 12, 1];
-
-  for(let [i, el] of barsEl.entries()) {
-    el.querySelector('.bar__progress').value = skills[i];
+  for(let barProgressElItem of barsProgressEl) {
+    const itemSkillsValue = parseInt(barProgressElItem.textContent);
+    barProgressElItem.value = itemSkillsValue;
   }
 }
 
