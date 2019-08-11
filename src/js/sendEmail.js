baa05a206e7b4c1c198accf095bfea9638677c53
tomi0505 <tomaszgrzyb.online@gmail.com>
2019-08-11 19:40:11 +0200

    sending e-mail works.

--- src/js/sendEmail.js
+++ src/js/sendEmail.js
@@ -4,6 +4,8 @@ const sendEmail = function() {
   const allInputs = sendMessageForm.querySelectorAll('.send-message-form__user-name-input, .send-message-form__user-email-input, .send-message-form__user-message-input');
   const userEmailInput = document.querySelector('.send-message-form__user-email-input');
 
+  const sendFormChecker = [];
+
   sendMessageForm.addEventListener('submit', function(e) {
     e.preventDefault();
 
@@ -23,6 +25,31 @@ const sendEmail = function() {
         }
       }
     }
+
+    // WILL I CAN SEND THE EMAIL[?]
+    sendFormChecker.length = 0;
+
+    for(let inputItem of allInputs) {
+      sendFormChecker.push(inputItem.classList.contains('error-validate'));
+    }
+
+    const resultSendFormChecker = sendFormChecker.every(item => item === false);
+
+    // WILL I CAN SEND THE EMAIL[TRUE/FALSE]
+    if(resultSendFormChecker) {
+      const formData = new FormData();
+      for(let i = 0; i < allInputs; i++) {
+        formData.append(allInputs[0].name, allInputs[0].value);
+      }
+
+      fetch(sendMessageForm.getAttribute('action'), {
+        method: 'POST',
+        body: formData
+      }).then(function(resp) {
+        console.log(resp.text());
+      })
+    }
+
   }, false);
 };
 
--- src/php/send-email.php
+++ src/php/send-email.php
@@ -1,9 +1,59 @@
-<?php 
+<?php
 
-$userName = $_POST['user-name'];
-$userEmail = $_POST['user-email'];
-$userMessage = $_POST['user-message'];
+$mailToSend = 'kontakt@tomi0505.kylos.pl';
 
-echo "Twoja wiadomość została wysłana pomyślnie. ".$userName;
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['user-name'];
+    $email = $_POST['user-email'];
+    $message = $_POST['user-message'];
+    $errors = Array();
+	$return = Array();
+
+    if (empty($name)) {
+        array_push($errors, 'name');
+    }
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        array_push($errors, 'email');
+    }
+    if (empty($message)) {
+        array_push($errors, 'message');
+    }
+    if (count($errors) > 0) {
+        $return['errors'] = $errors;
+    } else {
+        $headers  = 'MIME-Version: 1.0' . "\r\n";
+        $headers .= 'Content-type: text/html; charset=UTF-8'. "\r\n";
+        // $headers .= 'Access-Control-Allow-Origin: *' . "\r\n";
+        // $headers .= "Access-Control-Allow-Credentials: true";
+        $headers .= 'From: '.$email."\r\n";
+        $headers .= 'Reply-to: '.$email;
+        $message  = "
+            <html>
+                <head>
+                    <meta charset=\"utf-8\">
+                </head>
+                <style type=\"text/css\">
+                    body {font-family:sans-serif; padding:20px;}
+                    div {margin-bottom:10px;}
+                    .msg-title {margin-top:30px;}
+                </style>
+                <body>
+                    <div>Imię: $name</div>
+                    <div>Email: <a href=\"mailto:$email\">$email</a></div>
+                    <div>Wiadomość:</div>
+                    <div>$message</div>
+                </body>
+            </html>";
+
+        if (mail($mailToSend, 'Wiadomość ze strony - ' . date("d-m-Y"), $message, $headers)) {
+            $return['status'] = 'ok';
+        } else {
+            $return['status'] = 'error';
+        }
+    }
+
+    header('Content-Type: application/json');
+    echo json_encode($return);
+}
 
 ?>
