552872b7b0694a7a2005199ef9c29e5d79c15dd7
tomi0505 <tomaszgrzyb.online@gmail.com>
2019-08-14 00:02:48 +0200

    A hamburger btn animation works.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './scss/style.scss';
 import chromeMobile100Vh from './js/chromeMobile100Vh';
+import hamburgerBtn from "./js/hamburgerBtn";
 import scrollToAnchor from "./js/scrollToAnchor";
 import slideToLeftAnimation from "./js/slideToLeftAnimation";
 import slideToCenterAnimation from "./js/slideToCenterAnimation";
@@ -11,6 +12,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
   chromeMobile100Vh();
   window.addEventListener('resize', chromeMobile100Vh, false);
+
+  hamburgerBtn();
 
   copyrightYearEl.innerHTML = new Date().getFullYear();
 
--- src/js/hamburgerBtn.js
+++ src/js/hamburgerBtn.js
@@ -1,15 +1,13 @@
 const hamburgerBtn = function() {
   const hamburgerBtn = document.querySelector('.hamburger-btn');
-  const hamburgerBtnInnerLineItems = document.querySelectorAll('.hamburger-btn__inner-line-item');
 
   hamburgerBtn.addEventListener('click', function() {
     this.classList.toggle('active');
     setTimeout(() => {
-      for(let innerLineItem of hamburgerBtnInnerLineItems) {
-        innerLineItem.classList.toggle('clicked');
-      }
+      hamburgerBtn.firstElementChild.classList.toggle('clicked');
+      hamburgerBtn.lastElementChild.classList.toggle('clicked');
     }, 400);
   }, false);
-}
+};
 
 export default hamburgerBtn;
