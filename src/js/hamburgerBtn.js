91a843601cf2553e797d0fd8dea15183fc97285f
tomi0505 <tomaszgrzyb.online@gmail.com>
2019-08-14 01:51:59 +0200

    exp 1.

--- src/js/hamburgerBtn.js
+++ src/js/hamburgerBtn.js
@@ -1,5 +1,8 @@
+let menuIsVisible = false;
+
 const hamburgerBtn = function() {
   const hamburgerBtn = document.querySelector('.hamburger-btn');
+  const mainMenu = document.querySelector('.main-menu');
 
   hamburgerBtn.addEventListener('click', function() {
     this.classList.toggle('active');
@@ -7,6 +10,16 @@ const hamburgerBtn = function() {
       hamburgerBtn.firstElementChild.classList.toggle('clicked');
       hamburgerBtn.lastElementChild.classList.toggle('clicked');
     }, 400);
+
+    mainMenu.classList.toggle('active');
+
+    if(!menuIsVisible) {
+      setTimeout(() => {
+        mainMenu.classList.add('reset-transition');
+      }, 2000);
+
+      menuIsVisible = true;
+    }
   }, false);
 };
 
