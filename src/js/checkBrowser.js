d309e9940e920488870c88c8176befafbd2ee0fc
tomi0505 <tomaszgrzyb.online@gmail.com>
2019-10-30 20:00:26 +0100

    if ie test 3.

--- src/js/checkBrowser.js
+++ src/js/checkBrowser.js
@@ -1,8 +1,7 @@
 const checkBrowser = function() {
-  if (navigator.appName == 'Microsoft Internet Explorer' ||  !!(navigator.userAgent.match(/Trident/) || navigator.userAgent.match(/rv:11/)) || (typeof $.browser !== "undefined" && $.browser.msie == 1))
-  {
-    document.innerHTML = 'Twoja przeglądarka nie obsługuje tej strony internetowej. Skorzystaj proszę z przeglądarki <a href="http://www.google.com/intl/pl_pl/chrome">Chrome</a>.';
-  }
+  const isIE = /*@cc_on!@*/false || !!document.documentMode;
+
+  isIE ? document.innerHTML = 'Twoja przeglądarka nie obsługuje tej strony internetowej. Skorzystaj proszę z przeglądarki <a href="http://www.google.com/intl/pl_pl/chrome">Chrome</a>.' : "not IE 6-11.";
 };
 
 export default checkBrowser;
