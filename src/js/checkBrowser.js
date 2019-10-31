2842e9cfdf054c0ca2e18ad8effe26ac1c3c787d
tomi0505 <tomaszgrzyb.online@gmail.com>
2019-10-31 14:40:28 +0100

    _

--- src/js/checkBrowser.js
+++ src/js/checkBrowser.js
@@ -1,6 +1,6 @@
 const checkBrowser = function() {
   const ua = window.navigator.userAgent;
-  const isIE = /MSIE|Trident|Edge\//.test(ua);
+  const isIE = /MSIE|Trident\//.test(ua);
   
   if (isIE) {
     document.innerHTML = 'Twoja przeglądarka nie obsługuje nowoczesnych technologii. Pobierz <a href="http://www.google.com/intl/pl_pl/chrome">Chrome</a>.';
