edd60c814d83f2101b6c31dd86ec50cf15f88136
tomi0505 <tomaszgrzyb.online@gmail.com>
2019-10-31 14:38:07 +0100

    ie test 4

--- src/js/checkBrowser.js
+++ src/js/checkBrowser.js
@@ -1,7 +1,9 @@
 const checkBrowser = function() {
-  if (navigator.appName == 'Microsoft Internet Explorer' ||  !!(navigator.userAgent.match(/Trident/) || navigator.userAgent.match(/rv:11/)) || (typeof $.browser !== "undefined" && $.browser.msie == 1))
-  {
-    document.innerHTML = 'Twoja przeglądarka nie obsługuje tej strony internetowej. Skorzystaj proszę z przeglądarki <a href="http://www.google.com/intl/pl_pl/chrome">Chrome</a>.';
+  const ua = window.navigator.userAgent;
+  const isIE = /MSIE|Trident|Edge\//.test(ua);
+  
+  if (isIE) {
+    document.innerHTML = 'Twoja przeglądarka nie obsługuje nowoczesnych technologii. Pobierz <a href="http://www.google.com/intl/pl_pl/chrome">Chrome</a>.';
   }
 };
 
