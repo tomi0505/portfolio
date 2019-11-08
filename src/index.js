034ca475937a9e4fb45aab81e681ef346f9c81a1
tomi0505 <tomaszgrzyb.online@gmail.com>
2019-11-08 18:54:56 +0100

    check-browser-alert

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import './scss/style.scss';
-import checkBrowser from "./js/checkBrowser";
 import chromeMobile100Vh from './js/chromeMobile100Vh';
 import hamburgerBtn from "./js/hamburgerBtn";
 import scrollToAnchor from "./js/scrollToAnchor";
@@ -11,7 +10,19 @@ import sendEmail from "./js/sendEmail";
 document.addEventListener("DOMContentLoaded", () => {
   const copyrightYearEl = document.querySelector('.copyright__year');
 
-  checkBrowser();
+  (function() {
+    function isIE() {
+      var ua = window.navigator.userAgent;
+      var msie = ua.indexOf('MSIE ');
+      var trident = ua.indexOf('Trident/');
+
+      return (msie > 0 || trident > 0);
+    }
+
+    if(isIE()) {
+      document.innerHTML = '<aside class="check-browser-alert"><p>Twoja przeglądarka nie obsługuje nowoczesnych technologii webowych. Pobierz <a href="http://www.google.com/intl/pl_pl/chrome">Chrome</a>.</p></aside>';
+    }
+  }());
 
   chromeMobile100Vh();
   window.addEventListener('resize', chromeMobile100Vh, false);
