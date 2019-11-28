684f1b93e8b5cffd2d4cce5eb55a41335fc399dd
tomi0505 <tomaszgrzyb.online@gmail.com>
2019-11-28 21:37:38 +0100

    ifIE fixed _

--- src/index.js
+++ src/index.js
@@ -10,20 +10,6 @@ import sendEmail from "./js/sendEmail";
 document.addEventListener("DOMContentLoaded", () => {
   const copyrightYearEl = document.querySelector('.copyright__year');
 
-  (function() {
-    function isIE() {
-      var ua = window.navigator.userAgent;
-      var msie = ua.indexOf('MSIE ');
-      var trident = ua.indexOf('Trident/');
-
-      return (msie > 0 || trident > 0);
-    }
-
-    if(isIE()) {
-      document.innerHTML = '<aside class="check-browser-alert"><p>Twoja przeglądarka nie obsługuje nowoczesnych technologii webowych. Pobierz <a href="http://www.google.com/intl/pl_pl/chrome">Chrome</a>.</p></aside>';
-    }
-  }());
-
   chromeMobile100Vh();
   window.addEventListener('resize', chromeMobile100Vh, false);
 
