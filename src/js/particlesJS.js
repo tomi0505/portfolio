3c818826ae7c26f6b9907bdb1fd88b6e7c70d12d
tomi0505 <tomaszgrzyb.online@gmail.com>
2020-04-01 17:03:23 +0200

    remove console.log #2

--- src/js/particlesJS.js
+++ src/js/particlesJS.js
@@ -1,7 +1,6 @@
 import './plugins/particles.js-master/particles';
 
 const particles = function() {
-  console.log(particlesJS);
   particlesJS.load('header', 'src/js/plugins/particles.js-master/particles.json', function() {
   });
 };
