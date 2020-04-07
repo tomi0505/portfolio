2742355ea2f555c41836be72cd0e3cca17c726c1
tomi0505 <tomaszgrzyb.online@gmail.com>
2020-04-07 11:00:29 +0200

    I have removed the typo

--- src/js/particlesJS.js
+++ src/js/particlesJS.js
@@ -1,8 +1,7 @@
 import './plugins/particles.js-master/particles';
 
 const particles = function() {
-  particlesJS.load('header', 'src/js/plugins/particles.js-master/particles.json', function() {
-  });
+  particlesJS.load('header', 'src/js/plugins/particles.js-master/particles.json');
 };
 
 export default particles;
